6b8b50dba269b8b6fb7a5d687cc54611274e1fd9
cookieinbprep <142258312+cookieinbprep@users.noreply.github.com>
2023-08-21 11:47:17 -0700

    bugs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const quiz = document.getElementById("quiz");
 form.addEventListener("submit", submitted);
-
-function submitted(event) {
-    event.preventDefault();
-    if(answers.question1) {
-        return "Minecraft is the game recommended for you.";
-    }
+document.addEventListener("DOMContentLoaded", function()) {
+    const submitButton = document.getElementById("submit-btn");
+    const resultsSection = document.getElementById("result");
+}
+submitButton.addEventListener ("click", function ()){
+    const result = "Minecraft is the game recommended for you!";
 }
