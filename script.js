c47b2dc05b5dc06463906962094922eacecaa1dd
cookieinbprep <142258312+cookieinbprep@users.noreply.github.com>
2023-08-21 11:29:00 -0700

    quiz

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ form.addEventListener("submit", submitted);
 
 function submitted(event) {
     event.preventDefault();
-    if(answers.question1, answers.question2, answers.question3) {
+    if(answers.question1) {
         return "Minecraft is the game recommended for you.";
     }
 }
